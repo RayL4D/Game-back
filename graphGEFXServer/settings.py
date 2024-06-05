6bc92a1a8a0dacd2149380c4a5fa5781781b5c00
RayL4D <rayan.ladrait@gmail.com>
2024-06-05 15:36:00 +0200

    commit_init5

--- graphGEFXServer/settings.py
+++ graphGEFXServer/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework_swagger',
     'drf_yasg',
+    'corsheaders',
 
 
 
@@ -56,7 +57,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'graphGEFXServer.urls'
 
